088931c9cbd0e63a65c83000e1b34eb15299f2f8
崚 田村 <ao.megane.0716@gmail.com>
2018-09-16 19:46:01 +0900

    ぱーぺき

--- Lost/Lost/main.cpp
+++ Lost/Lost/main.cpp
@@ -12,9 +12,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	SetGraphMode(2160, 1440, 32);
 
 	{
-		/*SetWindowSizeChangeEnableFlag(TRUE);
+		SetWindowSizeChangeEnableFlag(TRUE);
 		SetWindowSizeExtendRate(0.6);
-		ChangeWindowMode(TRUE);*/
+		ChangeWindowMode(TRUE);
 		SetBackgroundColor(0, 0, 0);
 	}
 
